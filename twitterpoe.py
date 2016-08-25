a851b6f331a508c7ae626e5e6af06bf298108fce
Brian Caulkins <briancaulkins@gmail.com>
2016-08-25 00:12:11 -0500

    remove spaces between stanzas for theraven.txt

--- twitterpoe.py
+++ twitterpoe.py
@@ -1,6 +1,23 @@
-import sys
-poem = "theraven.txt"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open (poem) as rf:
-    for line in rf:
-        print (line)
+import tweepy, time, sys
+
+argfile = str(sys.argv[1])
+
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = '1234...'#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = '1234...'#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = '1234...'#keep the quotes, replace this with your access token
+ACCESS_SECRET = '1234...'#keep the quotes, replace this with your access token secret
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+filename=open(argfile,'r')
+f=filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(line + " #theraven #twitterpoe #twitterbot")
+    time.sleep(900)#Tweet every 15 minutes
